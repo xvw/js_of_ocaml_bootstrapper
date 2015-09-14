45f82c5f9e031fcf8fe2871e1bda232e419f4eb0
grimimi <grimfw@gmail.com>
2015-09-14 16:56:07 +0200

    test image

--- examples/canvassample.ml
+++ examples/canvassample.ml
@@ -7,20 +7,9 @@ module App =
         
     let initialize () =
       let app = Get.byId "application" in
+      let img = Canvas.image ~path:(Some "https://avatars2.githubusercontent.com/u/6772534?v=3&s=460") () in
       let _ = Canvas.create_in app 600 400 in
-      let _ =
-        Canvas.(
-          fill_all
-            (
-              linear_gradient
-                (point 75 100)
-                (point 5 90)
-                [
-                  (0., Color.red);
-                  (1., Color.white);
-                ]
-            )
-        )
+      let _ = Canvas.draw_image img (Canvas.point 10 10)
       in ()
                   
   end
