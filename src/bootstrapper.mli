7c8c4eb1c2f089e0ebbe32e8de85828d1e586174
grimimi <grimfw@gmail.com>
2015-10-14 02:29:16 +0200

    Append Interval primitives

--- src/bootstrapper.mli
+++ src/bootstrapper.mli
@@ -100,10 +100,28 @@ sig
     'a
 
   (** [Event.delayed_loop ~delay:2.0 f] execute [f] each 2 second *)
-  val delayed_loop :
-    ?delay:float -> (unit -> 'a) -> 'b Lwt.t 
+  val delayed_loop : ?delay:float -> (unit -> 'a) -> 'b Lwt.t 
    
   
+end
+
+module Interval :
+sig
+
+  (** Provide LWT's interval*)
+  
+  (** [Interval.set ~delay:2.0 f] execute [f] each 2 second *)
+  val set : ?delay:float -> (unit -> 'a) -> unit Lwt.t
+
+  (** [Interval.set_until ~delay:2.0 predicat f] execute [f] each 2 second 
+      until pred(i) (where i is the counter) returns true
+  *)
+  val set_until : ?delay:float -> (int -> bool) -> (unit -> 'a) -> unit Lwt.t
+
+  (** [Interval.set_for ~delay:2.0 10 f] execute [f] 10 each 2 second 
+  *)
+  val set_for : ?delay:float -> int -> (unit -> 'a) -> unit Lwt.t
+      
 end
 
 module Get :
