937efe386496806cf173327eb399b17599e2721b
xvw <xavier.vdw@gmail.com>
2015-11-02 03:09:31 +0100

    Append Interval interface

--- src/bootstrapper.ml
+++ src/bootstrapper.ml
@@ -13,26 +13,6 @@ module Color    = BootColor
 module Canvas   = BootCanvas
 module Storage  = BootStorage
 
-
-module Interval =
-struct
-
-  let set ?(delay=0.1) f =
-    Event.delayed_loop ~delay:delay f
-
-  let set_until ?(delay=0.1) pred f =
-    let cpt = ref 0 in
-    let _ = while pred(!cpt) do
-        let _ = Lwt_js.sleep delay >>= (fun _ -> Lwt.return (f ())) in
-        cpt := !cpt + 1
-      done
-    in Lwt.return_unit
-    
-  let set_for ?(delay=0.1) x f =
-    set_until ~delay:delay (fun i -> i < x) f
-         
-end
-
 module Get =
 struct
 
