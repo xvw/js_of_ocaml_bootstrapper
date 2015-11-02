2a733fc8582ee7141f9338acbfb15d98b46a53d6
xvw <xavier.vdw@gmail.com>
2015-11-02 02:48:42 +0100

    clean builded files

--- _build/src/bootPervasives.ml
+++
@@ -1,7 +0,0 @@
-exception Element_not_found
- 
-let ( >>= )   = Lwt.bind
-let _s        = Js.string
-let s_        = Js.to_string
-let alert x   = Dom_html.window ## alert (_s x)
-let log x     = Firebug.console ## log(x)
