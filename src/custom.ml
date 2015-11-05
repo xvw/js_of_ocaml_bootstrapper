da580906a403f20d442f0af4cedf652dd40bb09c
xvw <xavier.vdw@gmail.com>
2015-11-05 01:17:44 +0100

    Make todo list

--- src/custom.ml
+++ src/custom.ml
@@ -14,6 +14,8 @@ let task title label state = {
 ; state = state
 }
 
+let change_state t s = { t with state = s }
+
 let task_tojson = Yojson.to_string<task>
 let task_ofjson = Yojson.from_string<task>
 
@@ -35,6 +37,68 @@ struct
     let label = Html.input_create ~into:(Some container) "text" "Label" in
     let press = Html.input_create ~into:(Some container) "button" "record" in
     (title, label, press)
+
+  let task_class task =
+    let f = if task.state then "finished" else "not-finished" in
+    ["a_task"; f]
+
+  let swap_btn task btn =
+    if task.state then
+      btn ## value <- (Js.string "Reprendre")
+    else btn ## value <- (Js.string "Terminer")
+
+  let create_block_task ul id js_task =
+    let task = task_ofjson js_task in
+    let li = Html.element
+        ~id:(Some id)
+        ~classes:(task_class task)
+        Dom_html.createLi
+    in
+    let title = Html.element
+        ~classes:["a_title"]
+        ~into:(Some li)
+        Dom_html.createSpan
+    in 
+    let label = Html.element
+        ~classes:["a_label"]
+        ~into:(Some li)
+        Dom_html.createSpan
+    in
+    let end_task = Html.input_create ~into:(Some li) "button" "Terminer" in
+    let _ = swap_btn task end_task in
+    let raz_task = Html.input_create ~into:(Some li) "button" "Supprimer" in
+    let _ =
+      Event.(
+        end_task >- (
+          click, fun _ _ ->
+            match Storage.Local.get id with
+            | None -> ()
+            | Some thing ->
+              let rtask = task_ofjson thing in
+              let new_task = (change_state task (not rtask.state)) in 
+              let _ =
+                Storage.Local.set
+                  id (new_task |> task_tojson)
+              in
+              let _ = Html.remove_classes li ["finished"; "not-finished"] in
+              let _ = Html.add_classes li (task_class new_task) in
+              swap_btn new_task end_task
+        )
+      )
+    in 
+    let _ =
+      Event.(
+        raz_task >- (
+          click, fun _ _ ->
+            let _ = Storage.Local.remove id in
+            Dom.removeChild ul li
+        )
+      )
+    in 
+    let _ = Html.text ~into:(Some title) task.title in
+    let _ = Html.text ~into:(Some label) task.label in
+    Html.(ul <+> li)
+         
     
 end
 
@@ -51,7 +115,11 @@ let record_task t l =
   in ()
 
 let rewrite_ul ul =
-  let childs = ul 
+  let _ = Html.remove_children ul in
+  let hash = Storage.Local.to_hashtbl () in
+  Hashtbl.iter (fun key value ->
+      Gui.create_block_task ul key value
+    ) hash
     
 let bind_event e t l u  =
   Event.(
@@ -72,6 +140,7 @@ module Todolist =
       let (task_title, task_label, task_press) = Gui.create_form app in
       let list  = Gui.create_list app in
       let _ = bind_event task_press task_title task_label list in
+      let _ = rewrite_ul list in
       ()
         
   end
